943e84413cd3f551aa44ca680c7697ce9f6bd275
unn4m3d <smelnikov871@gmail.com>
2017-06-19 01:11:53 +1000

    Make texture coords and normals nilable

--- src/models.cr
+++ src/models.cr
@@ -4,7 +4,7 @@ require "crystaledge"
 module Models
   alias Vec3 = CrystalEdge::Vector3
 
-  record Vertex, coord : Vec3, texcoord : Vec3, normal : Vec3
+  record Vertex, coord : Vec3, texcoord : Vec3?, normal : Vec3?
 
   alias Face = Array(Vertex)
 
