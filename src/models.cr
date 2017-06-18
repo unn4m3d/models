8ba34df63e5b00e1d78a101f7fd621c63238c713
unn4m3d <smelnikov871@gmail.com>
2017-06-19 01:10:38 +1000

    Remove color field from Vertex\

--- src/models.cr
+++ src/models.cr
@@ -4,9 +4,9 @@ require "crystaledge"
 module Models
   alias Vec3 = CrystalEdge::Vector3
 
-  record Vertex, coord : Vec3, color : Vec3, texcoord : Vec3, normal : Vec3
+  record Vertex, coord : Vec3, texcoord : Vec3, normal : Vec3
 
   alias Face = Array(Vertex)
 
-  record Shape, name : String, faces : Array(Face), subshapes : Array(Shape)
+  record Shape, name : String, faces : Array(Face), material : String, subshapes : Array(Shape)
 end
